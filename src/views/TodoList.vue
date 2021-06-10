73d23c097c34038c2c8f930b55ff19b499469ce0
LiMin <a0952579528@gmail.com>
2021-06-11 00:36:14 +0800

    todolist

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -10,10 +10,10 @@
         class="form-control"
         placeholder="準備要做的任務"
         v-model="newTodo"
-        @keyup.enter="addTodo()"
+        @keyup.enter="addTodo"
       />
       <div class="input-group-append">
-        <button class="btn btn-primary" type="button" @click="addTodo()">
+        <button class="btn btn-primary" type="button" @click="addTodo">
           新增
         </button>
       </div>
@@ -139,12 +139,19 @@ export default {
       }
       const todoId = Math.floor(Date.now());
       const todoTitle = this.newTodo.trim();
-      const todoObj = {
-        id: todoId,
-        title: todoTitle,
-        completed: false,
-      };
-      this.todos.push(todoObj);
+      this.todos = [
+        ...this.todos,
+        {
+          id: todoId,
+          title: todoTitle,
+          completed: false,
+        },
+      ];
+      // this.todos.push({
+      //   id: todoId,
+      //   title: todoTitle,
+      //   completed: false,
+      // });
 
       this.newTodo = '';
       this.setLocalStorage();
