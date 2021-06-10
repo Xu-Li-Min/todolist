ca309270bd3abbda5c5b234d0491f7c8b34bf814
LiMin <a0952579528@gmail.com>
2021-06-11 01:23:18 +0800

    bootstrap scss custom file

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="container">
-    <h1 class="text-center">這是您今天的代辦事項！</h1>
+    <h1 class="text-center">這是您今天的代辦事項</h1>
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
-@import '../node_modules/bootstrap/scss/bootstrap';
 @import './assets/scss/all.scss';
 </style>
